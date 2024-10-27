2879371411c13e9fefeccd0da821e8cf30291c4b
l0ix <l0ix@protonmail.com>
2024-10-27 21:41:45 +0000

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "splunk_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (free-tier eligible)
+  instance_type = "t2.micro"
+  key_name      = "your-key-name"
+
+  tags = {
+    Name = "SplunkInstance"
+  }
+
+  # Security Group to allow SSH and HTTP
+  vpc_security_group_ids = [aws_security_group.splunk_sg.id]
+}
+
+resource "aws_security_group" "splunk_sg" {
+  name        = "splunk_security_group"
+  description = "Allow SSH and HTTP access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${cidrsubnet(var.ddns_name, 32)}"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["${cidrsubnet(var.ddns_name, 32)}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "ec2_instance_public_ip" {
+  value = aws_instance.splunk_instance.public_ip
+}
